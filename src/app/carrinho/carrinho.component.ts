eb27256fb52d9bcad32542b862aa5d14bc2f8250
Renato <rfbellani.estudo@gmail.com>
2019-11-27 02:59:21 -0300

    Ajuste Tela Carrinho Compras

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -9,9 +9,14 @@ const API_URL = 'http://localhost:8080/api';
     templateUrl: './carrinho.component.html'
 })
 export class CarrinhoComponent  implements OnInit{
-    itens: Object[] = [];
-    itens_aux: Object[] = [];
+    carrinhos: Object[] = [];
+    usuarios: Object[] = [];
+    carrinhos_aux: Object[] = [];
+    itensCarrinho: Object[] = [];
     carrinhoForm: FormGroup;
+    carrinhoEncontrado = false;
+    clienteEncontrado = false;
+    itens:  Object[] = [];
 
     constructor(
       private http: HttpClient,
@@ -27,7 +32,7 @@ export class CarrinhoComponent  implements OnInit{
 
     ngOnInit(): void {
         this.carrinhoForm = this.formBuilder.group({
-          email: ['', Validators.required],
+          email: ['', [Validators.required ,Validators.email] ]
 
         });
     }
@@ -37,4 +42,86 @@ export class CarrinhoComponent  implements OnInit{
         console.log('Vai Direcionar para Menu...');
         this.router.navigateByUrl('menu');
     }
+
+    cliente (){
+      console.log('Entrou em Cliente...');
+
+      const email = this.carrinhoForm.get('email').value;
+
+      console.log('Vai Consultar...' + email);
+
+      console.log(this.http);
+
+      console.log(API_URL + '/usuario?email=' + email);
+
+      //Obter o ID do Item
+      const observable = this.http.get<Object[]>( API_URL + '/usuario?email=' + email);
+      
+      observable.subscribe(usuarios => {
+        console.log(usuarios); 
+        this.usuarios = usuarios; 
+        console.log(this.usuarios);
+        const usuario = this.usuarios[0];
+        
+        if (usuario != null){
+           console.log('Usuario Encontrado.. Vai Consultar Carrinho');
+
+           console.log(API_URL + '/carrinho?email=' + email);
+
+           this.clienteEncontrado = true;
+
+           const observable = this.http.get<Object[]>( API_URL + '/carrinho?email=' + email);
+      
+           observable.subscribe(carrinhos => {
+                console.log(carrinhos); 
+                this.carrinhos = carrinhos; 
+                console.log(this.carrinhos);
+                const carrinho = this.carrinhos[0];  
+
+                if (carrinho != null){
+                  console.log('Carrinho Encontrado.. Vai formatar Lista Itens');
+                  
+                  this.itensCarrinho = (<any>this.carrinhos[0]).itens;
+                  this.carrinhoEncontrado = true;
+
+                  console.log(this.itensCarrinho);
+
+                } else {
+                  console.log('Carrinho não Encontrado..');                  
+                }
+                
+           });
+
+
+        } else {
+
+          console.log('Usuario não Encontrado..');
+        }
+
+        //const id =  (<any>this.usuarios[0]).id;
+        //console.log('ID --> ' + id);
+
+        //this.http.patch(API_URL + '/item/' + id , { nome, valor } ).subscribe(
+        //  () => console.log('Alterado com sucesso'),
+        //  err => {
+        //      console.log(err);
+        //      this.itemForm.reset();
+        //  });
+
+      });
+  }
+
+  adicionarItem(){
+    console.log('Adicionar Item..');
+
+    const item = this.carrinhoForm.get('selItem').value;
+
+    console.log('Item: ' + item);
+
+
+  }
+
+  removerItem(){
+    console.log('Remover Item..');
+  }
 }
